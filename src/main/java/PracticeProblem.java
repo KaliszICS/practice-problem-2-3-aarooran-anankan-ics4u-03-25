1c935b34758f74395a14af897566c4cc1fa61855
aarooran-anankan-ics4u-03-25 <aarooran.anankan@student.tdsb.on.ca>
2025-03-20 23:02:09 +0000

    Final Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,24 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static void main(String args[]) {}
 
-	}
+	public static void bubbleSortString(String[] strings) {
+		
+		boolean swapped = true;
 
-	public static void q1() {
-		//Write question 1 code here
-	}
+		for (int j=0; j<strings.length-1 && swapped; j++) {
+			
+			swapped = false;
 
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
+			for (int i=0; i<strings.length-1-j; i++) {
+				if (strings[i].compareToIgnoreCase(strings[i+1])>0) {
+					String temp = strings[i];
+					strings[i] = strings[i+1];
+					strings[i+1]=temp;
+					swapped=true;
+				}
+			}
+		}
 	}
 
 }
